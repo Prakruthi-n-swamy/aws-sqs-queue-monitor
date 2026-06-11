22b2e2adbb332def36ca151db10c25ba0c70ff8e
Prakruthi-n-swamy <prakruthihello123@gmail.com>
2026-06-11 13:14:41 +0530

    Add endpoint health check functionality

--- monitor/health_check.py
+++ monitor/health_check.py
@@ -1,1 +1,10 @@
+import requests
 
+def check_endpoint(url):
+
+    try:
+        response = requests.get(url, timeout=5)
+        return response.status_code
+
+    except Exception:
+        return "FAILED"
